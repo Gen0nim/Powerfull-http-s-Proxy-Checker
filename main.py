e2a9d842eb723bf94daee03943cf564c2ff37631
Xin <70026238+XinOnGithub@users.noreply.github.com>
2021-04-23 03:40:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Dint Skid Bitch
+#Dont Skid Bitch
 #By XinOnGithub
 #https://www.youtube.com/channel/UC-8t8_KCMoX5YpjbivyeIYQ
 
@@ -8,86 +8,55 @@ from colorama import Fore, init, Style
 from os import system, name
 init()
 
-with open('config.json') as f:
-	cfg = json.load(f)
+def RFile(file,method):
+    with open(file,method) as file:
+        a = [line.strip('\n') for line in file]
+        return a
 
-Threads = cfg.get("Threads")
-Timeout = cfg.get("Timeout")
-Console_Details = cfg.get("Console_Details")
-Website = cfg.get("Website")
-
-def menu():
-    if name == 'posix':
-        system('clear')
-        system('clear')
-    elif name in ('ce', 'nt', 'dos'):
-        system('cls')
-        system('cls')
-    else:
-        print("Please Check config.json File")
-        exit()
-    banner = f"""
+def PrintLogo():
+    system("cls" if name == 'nt' else 'clear')
+    print(f"""
                                     {Fore.LIGHTMAGENTA_EX}╔═══════════════════════════════════════════════╗{Fore.RESET}
                                     {Fore.LIGHTMAGENTA_EX}║{Fore.RESET}   __   __   __             ___  __   __       {Fore.LIGHTMAGENTA_EX}║{Fore.RESET}
                                     {Fore.LIGHTMAGENTA_EX}║{Fore.RESET}  |__) |__) /  \ \_/ \ /     |  /  \ /  \ |    {Fore.LIGHTMAGENTA_EX}║{Fore.RESET}
                                     {Fore.LIGHTMAGENTA_EX}║{Fore.RESET}  |    |  \ \__/ / \  |      |  \__/ \__/ |___ {Fore.LIGHTMAGENTA_EX}║{Fore.RESET}
                                     {Fore.LIGHTMAGENTA_EX}║{Fore.RESET}                                               {Fore.LIGHTMAGENTA_EX}║{Fore.RESET}
                                     {Fore.LIGHTMAGENTA_EX}╚═══════════════════════════════════════════════╝{Fore.RESET}
-                                             {Fore.LIGHTMAGENTA_EX}  A PowerFull Proxy Checker ! {Fore.RESET}
-    """
-    print(banner)
+                                             {Fore.LIGHTMAGENTA_EX}  A PowerFull Proxy Checker ! {Fore.RESET}""")
 
-def RFile(file,method):
-    with open(file,method) as file:
-        a = [line.strip('\n') for line in file]
-        return a
 
-def CheckProxy(proxy, Website):
+
+def CheckProxy(proxy, Timeout):
 	try:
-		rqss = requests.get(Website, proxies={'http': 'http://'+proxy, 'https': 'http://'+proxy},timeout=Timeout)
-		system(f"title [ Most Powerfull Proxy Checker ] ~ Threads {active_count()-1}")
-		if Console_Details == "True":
-			print(f"{Fore.LIGHTGREEN_EX}[GOOD] {proxy}\n")
-		else:
-			pass
-		valid = open('Result/Good.txt', 'a')
-		valid.write(proxy+"\n")
+		requests.get('https://www.google.com/', proxies={'http': f'http://{proxy}', 'https': f'https://{proxy}'}, timeout=Timeout)
+		print(f"{Fore.LIGHTGREEN_EX}[GOOD] {proxy}\n")
+		valid = open('Result/Good.txt', 'a').write(proxy+"\n")
 		valid.close()
 	except:
-		system(f"title [ Most Powerfull Proxy Checker ] ~ Threads {active_count()-1}")
-		if Console_Details == "True":
-			print(f"{Fore.RED}[BAD] {proxy}\n")
-		else:
-			pass
-		bad = open('Result/Dead.txt', 'a')
-		bad.write(proxy+"\n")
-		bad.close()
+		print(f"{Fore.RED}[BAD] {proxy}\n")
 
-def Runner(Website,Threads,Timeout,Console_Details):
-    menu()
+def Runner():
+    Timeout = int(input("Timeout :"))
+    Threads = int(input("Threads :"))
     proxy_list = RFile('Proxy.txt','r')
-    proxy_count = len(proxy_list)
-    print(f"Loaded {proxy_count} proxy")
-    print(f"Config details : Website : {Website} | Threads : {Threads} | Timeout : {Timeout} | Console Details : {Console_Details}\nPress enter to start!")
-    system("pause >nul")
-    time.sleep(1)
+    time.sleep(2)
     for proxy in proxy_list:
         a = True
         while a:
             if active_count() <= Threads:
-                Thread(target=CheckProxy,args=(proxy, Website, )).start()
+                Thread(target=CheckProxy,args=(proxy, Timeout)).start()
                 a = False
 
 if __name__ == "__main__":
     try:
-        Runner(Website,Threads,Timeout,Console_Details)
+        PrintLogo()
+        Runner()
         time.sleep(3)
-        menu()
-        print(Fore.GREEN,"Checked all proxy with succes.")
+        PrintLogo()
+        print(f"{Fore.GREEN}Checked all proxy with succes.")
     except KeyboardInterrupt:
         print("Stopped by user")
     except IndexError:
         print("Put proxy in Proxy.txt file")
     except:
 	    print("Something went wrong")
-
